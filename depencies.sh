0fe901ef8d65bde19e5d7c6523d7d820ec2dfcb6
Miguel Lares <c.code.jp@gmail.com>
2016-03-27 12:06:51 -0600

    Pacman install

--- depencies.sh
+++ depencies.sh
@@ -3,3 +3,6 @@
 
 #Font symbola
 #wget -P tmp/ https://projects.archlinux.org/svntogit/community.git/snapshot/community-e28ff2ffb028ccd2233224bb6d124af12d655075.tar.gz 
+
+#Pacman
+pacman -S xf86-video-vesa mesa xorg-server xorg-utils xorg-xinit xorg-xrandr rofi git sudo fakeroot make binutils gcc perl-anyevent-i3 unzip terminus-font xorg-xset ranger ncmpcpp mpc mpd 
