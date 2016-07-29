21b97c10c7f9877d9cd22c0c9eac51de0f5e2a74
Miguel Lares <c.code.jp@gmail.com>
2016-07-29 17:04:40 +0000

    Update

--- depencies.sh
+++ depencies.sh
@@ -1,3 +1,14 @@
+#Pacman
+su
+pacman -S xf86-video-vesa mesa xorg-server xorg-utils xorg-xinit xorg-xrandr rofi git sudo fakeroot make binutils gcc 
+perl-anyevent-i3 unzip terminus-font xorg-xset ranger ncmpcpp mpc mpd rsync cmake vlc ntfs-3g xorg-xlsfonts netbeans htop scrot 
+lm_sensors xdotools ruby awk rfkill networkmanager network-manager-applet feh weechat xf86-input-synaptics file-roller rhythmbox 
+vim wget zsh alsa-utils
+
+pacman -S php mariadb php-apache php-mcrypt phpmyadmin php-intl php-imap php-sqlite
+
+exit
+
 #Fonts
 wget -P tmp/ https://aur.archlinux.org/cgit/aur.git/snapshot/ttf-font-awesome.tar.gz
 git clone https://github.com/powerline/fonts
@@ -5,15 +16,7 @@ git clone https://github.com/powerline/fonts
 #i3
 git clone https://github.com/Airblader/i3
 git clone https://github.com/vivien/i3blocks
-
-#Pacman
-pacman -S xf86-video-vesa mesa xorg-server xorg-utils xorg-xinit xorg-xrandr rofi git sudo fakeroot make binutils gcc 
-perl-anyevent-i3 unzip terminus-font xorg-xset ranger ncmpcpp mpc mpd rsync cmake vlc ntfs-3g xorg-xlsfonts netbeans htop scrot 
-lm_sensors xdotools ruby awk rfkill networkmanager network-manager-applet feh weechat xf86-input-synaptics file-roller rhythmbox 
-vim wget zsh alsa-utils
     
-pacman -S php mariadb php-apache php-mcrypt phpmyadmin php-intl php-imap php-sqlite
-
  mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
  mysql_secure_installation
 
